3b79ce991154ea5497e48cf5d1cc934a1e50f56d
Santiago072 <santiagolizcanosuarez@gmail.com>
2026-05-08 14:41:37 -0500

    Config Variables Hosting

--- config/conexion.php
+++ config/conexion.php
@@ -1,11 +1,17 @@
 <?php
 function conexion(){
-    $host = "sql10.freesqldatabase.com";
-    $user = "sql10825860";
-    $pass = "6rsS9peEn3";
-    $db = "sql10825860";
+    $host = "turntable.proxy.rlwy.net";
+    $user = "root";
+    $pass = "YpKcDtbHqHcfaQwq1nxVNHiQq0brrYgk";
+    $db = "railway";
+    $port = 52251;
 
-    $conexion = mysqli_connect($host, $user, $pass, $db);
+    $conexion = mysqli_connect($host, $user, $pass, $db, $port);
+
+    if (!$conexion) {
+        die("Error de conexión: " . mysqli_connect_error());
+    }
+    
     return $conexion;
 }
 ?>
