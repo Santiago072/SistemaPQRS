7a90b56e220ab2e3f759882d4cbb52594d75a2cc
Santiago072 <santiagolizcanosuarez@gmail.com>
2026-05-08 17:28:59 -0500

    fix: inicializar con SSL

--- config/conexion.php
+++ config/conexion.php
@@ -6,9 +6,11 @@ function conexion(){
     $db   = "railway";
     $port = 52251;
 
-    $conexion = mysqli_connect($host, $user, $pass, $db, $port);
-
-    if (!$conexion) {
+    // Inicializar con SSL
+    $conexion = mysqli_init();
+    mysqli_ssl_set($conexion, NULL, NULL, NULL, NULL, NULL);
+    
+    if (!mysqli_real_connect($conexion, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
         die("Error de conexión: " . mysqli_connect_error());
     }
     
