0b47adc2731c6e3d4f18f26424d750f2832e35b3
Santiago072 <santiagolizcanosuarez@gmail.com>
2026-05-14 13:51:01 -0500

    Conexion Local con BD XAMPP

--- config/conexion.php
+++ config/conexion.php
@@ -1,5 +1,5 @@
 <?php
-function conexion() {
+/* function conexion() {
     $host = 'mysql.railway.internal';
     $user = 'root';
     $pass = 'YpKcDtbHqHcfaQwqlnxVNHiQqObrrYgk';
@@ -10,4 +10,17 @@ function conexion() {
         die("Error de conexión: " . mysqli_connect_error());
     }
     return $conexion;
+} */
+function conexion() {
+    $host = 'localhost';
+    $user = 'root';
+    $pass = 'root';
+    $db = 'sistema_pqrs';
+
+    $conexion = mysqli_connect($host, $user, $pass, $db);
+    if (!$conexion) {
+        die("Error de conexión: " . mysqli_connect_error());
+    }
+    return $conexion;
 }
+?>
